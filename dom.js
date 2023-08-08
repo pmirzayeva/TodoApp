4ffb753840850dc7a8e1377eebac518c49b38ad0
pmirzayeva <pmirzayeva1@gmail.com>
2023-08-08 12:43:10 +0200

    create the files

---
+++ dom.js
@@ -0,0 +1,51 @@
+
+const todoInput=document.querySelector("#todoInput"),todoAdd=document.querySelector("#todoAdd"),todoList=document.querySelector("#todoList")
+
+let data=[]
+
+todoAdd.addEventListener("click",addList)
+
+todoInput.addEventListener("keydown",function(e){
+    if(e.key==="Enter"){
+        createTodo()
+    }
+});
+
+function addList() {
+    createTodo();
+  }
+
+function createTodo(){
+    const value=todoInput.value
+
+    data.push(value);
+    todoInput.value = "";
+    renderTodoList(data)
+
+}
+
+function removeTodo(e,todoIndex){
+    console.log(todoIndex);
+    todoAdd.removeEventListener("click", addList);
+    const newdata=data.filter((item,index)=>index !== todoIndex)
+    data=newdata;
+    renderTodoList(data)
+}
+
+
+function renderTodoList(arr){
+    const newContent=arr.map(function (todo, index) {
+        return `<li>
+        <span>${todo}</span><button class="btn border-0" onclick="removeTodo(this,${index})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+      </svg></button></li>`;
+      })
+      .join("");
+  
+
+
+      console.log(newContent);
+
+      todoList.innerHTML=newContent
+}
